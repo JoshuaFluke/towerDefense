81403067fd0fe8e12743d8357ec8ffb3cdbc6dd2
Joshua Fluke <jfluke717@gmail.com>
2017-06-08 18:24:01 -0600

    starting scripts

---
+++ Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int target = 0;
+    public Transform exitPoint;
+    public Transform[] wayPoints;
+    public float navigationUpdate;
+
+    private Transform enemy;
+    private float navigationTime = 0;
+
+    // Use this for initialization
+    void Start () {
+        enemy = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (wayPoints != null)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                if (target < wayPoints.Length)
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, wayPoints[target].position, 0.8f * navigationTime);
+                }
+                else
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, exitPoint.position, 0.8f * navigationTime);
+                }
+                navigationTime = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "CheckPoint")
+        {
+            target += 1;
+        }
+        else if (other.tag == "Finish")
+        {
+            GameManager.instance.removeEnemyFromScreen();
+            Destroy(gameObject);    
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    public GameObject spawnPoint;
+    public GameObject[] enemies;
+    public int maxEnemiesOnScreen;
+    public int totalEnemies;
+    public int enemiesPerSpawn;
+
+    private int enemiesOnScreen = 0;
+    // Use this for initialization
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start () {
+        spawnEnemy();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void spawnEnemy()
+    {
+        if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies) {
+            for (int i = 0; i < enemiesPerSpawn; i++)
+            {
+                if (enemiesOnScreen < maxEnemiesOnScreen)
+                {
+                    GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+                    newEnemy.transform.position = spawnPoint.transform.position;
+                    enemiesOnScreen += 1;
+                }
+            }
+        }
+    }
+
+    public void removeEnemyFromScreen()
+    {
+        if (enemiesOnScreen > 0)
+        {
+            enemiesOnScreen--;
+        }
+    }
+}
